d3f31825dce88357bfe4b99a9f6773335c794c47
itzan jimenez <holaitzan@gmail.com>
2025-10-09 21:31:04 +0200

    corregida permutacion

--- 04-Polialfabetic/src/Polialfabetic.java
+++ 04-Polialfabetic/src/Polialfabetic.java
@@ -1,93 +1,88 @@
-import java.util.Random;
+import java.util.*;
 
 public class Polialfabetic {
     static final char[] alfabet = "AÀÁBCÇDEÈÉFGHIÌÍïJKLMNOÒÓPQRSTUÚÙÜVWXYZ".toCharArray();
-    private static long clauSecreta = 300;
+    private static final long CLAUSECRETA = 8;
     private static Random randomNum;
     static String alfabetPermutat;
     
+    public static String xifraPoliAlfa(String msg) { return usaPoliAlfa(msg, true); }
+    public static String desxifraPoliAlfa(String msg) { return usaPoliAlfa(msg, false); }
 
-    public static String xifraPoliAlfa(String msg){
-        return usaPoliAlfa(msg, true);
-    }
-    public static String usaPoliAlfa(String msg, boolean bool){
-        String xifrat="";
+    public static String usaPoliAlfa(String msg, boolean xifrar){
+        String xifrat = "";
         for (int i = 0; i < msg.length(); i++){  
             permutaAlfabet();
+            char c = msg.charAt(i);
             boolean especial = true; 
-            if (Character.isLowerCase(msg.charAt(i))){
-                if(bool){
-                for(int j = 0; j < alfabet.length;j++){
-                    if(Character.toLowerCase(alfabet[j]) == msg.charAt(i)){
-                        xifrat = xifrat + Character.toLowerCase(alfabetPermutat.charAt(j));
-                        especial = false;
-                        break;
+            if (Character.isLowerCase(c)){
+                for(int j = 0; j < alfabet.length; j++){
+                    if (xifrar) {
+                        if(Character.toLowerCase(alfabet[j]) == c){
+                            xifrat += Character.toLowerCase(alfabetPermutat.charAt(j));
+                            especial = false;
+                            break;
+                        }
+                    } else { 
+                        if(Character.toLowerCase(alfabetPermutat.charAt(j)) == c){
+                            xifrat += Character.toLowerCase(alfabet[j]);
+                            especial = false;
+                            break;
+                        }
                     }
                 }
-                }else{
-                    for(int j = 0; j < alfabet.length;j++){
-                    if(Character.toLowerCase(alfabetPermutat.charAt(j)) == msg.charAt(i)){
-                        xifrat = xifrat + Character.toLowerCase(alfabet[j]);
-                        especial = false;
-                        break;
+            } 
+            else if (Character.isUpperCase(c)){
+                for(int j = 0; j < alfabet.length; j++){
+                    if (xifrar) {
+                        if(alfabet[j] == c){
+                            xifrat += alfabetPermutat.charAt(j);
+                            especial = false;
+                            break;
+                        }  
+                    } else { 
+                        if(alfabetPermutat.charAt(j) == c){
+                            xifrat += alfabet[j];
+                            especial = false;
+                            break;
+                        }  
                     }
-                }
-                }
-            }
-            else if (Character.isUpperCase(msg.charAt(i))){
-                if(bool){
-                for(int j = 0; j < alfabet.length;j++){
-                    if(alfabet[j] == msg.charAt(i)){
-                        xifrat = xifrat + alfabetPermutat.charAt(j);
-                        especial = false;
-                        break;
-                    }  
-                }}else{
-                    for(int j = 0; j < alfabet.length;j++){
-                    if(alfabetPermutat.charAt(j) == msg.charAt(i)){
-                        xifrat = xifrat + alfabet[j];
-                        especial = false;
-                        break;
-                    }  
-                }
                 }
             }
             if (especial){
-                xifrat = xifrat + msg.charAt(i);
+                xifrat += c;
             }      
         }
         return xifrat;
-    
     }
-    public static String desxifraPoliAlfa(String msg){
-        return usaPoliAlfa(msg, false);
-    }
-    public static void permutaAlfabet() {
-        for (int i = 0; i < alfabet.length; i++) {
-            alfabetPermutat = alfabetPermutat + alfabet[(i + randomNum.nextInt(alfabet.length)) % alfabet.length];
 
-        }
+    public static void permutaAlfabet() {
+        List<Character> llista = new ArrayList<>();
+        for (char c : alfabet) llista.add(c);
+        Collections.shuffle(llista, randomNum);
+        StringBuilder sb = new StringBuilder();
+        for (char c : llista) sb.append(c);
+        alfabetPermutat = sb.toString();
     }
-    
-    public static void initRandom(long clauSecreta){
-        randomNum = new Random(clauSecreta);
+
+    public static void initRandom(long CLAUSECRETA){
+        randomNum = new Random(CLAUSECRETA);
     }
 
     public static void main(String[] args) {
-
         String msgs[] = {"Test 01 àrbritre, coixí, Perímetre",
             "Test 02 Taüll, DÍA, año",
             "Test 03 Peça, Orrius, Bòvila"};
         String msgsXifrats[] = new String[msgs.length];
         System.out.println("Xifratge:\n--------");
         for (int i = 0; i < msgs.length; i++) {
-            initRandom(clauSecreta);
+            initRandom(CLAUSECRETA);
             msgsXifrats[i] = xifraPoliAlfa(msgs[i]);
             System.out.printf("%-34s -> %s%n", msgs[i], msgsXifrats[i]);
         }
         System.out.println("Desxifratge:\n-----------");
         for (int i = 0; i < msgs.length; i++) {
-            initRandom(clauSecreta);
+            initRandom(CLAUSECRETA);
             String msg = desxifraPoliAlfa(msgsXifrats[i]);
             System.out.printf("%-34s -> %s%n", msgsXifrats[i], msg);
         }
